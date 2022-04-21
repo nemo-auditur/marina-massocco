041d33c68de7777d3d900eb6734f21c8c9dc896d
Clément Legal <clement@macbook-pro-de-clement.home>
2022-04-21 17:28:39 +0200

    Header and contact responsive

--- pages/projects/[id].js
+++ pages/projects/[id].js
@@ -1,40 +1,5 @@
 import { connectToDatabase } from '../../lib/mongodb'
 
-export const getStaticPaths = async () =>  {
-
-    const { db } = await connectToDatabase();
-    const projects = await db
-      .collection("projects")
-      .find()
-      .limit(30)
-      .toArray();
-    
-      const paths = projects.map(project => {
-          return {
-              params : {id: project._id.toString()}
-          }
-      })
-    return {
-        paths,
-        fallback : false
-    }
-}
-
-
-export const getStaticProps = async (context) => {
-    const id = context.params.id
-
-    const { db } = await connectToDatabase();
-    const project = await db
-    .collection("projects")
-    .find({_id : id})
-    .limit(1)
-    .toArray();
-    return {
-        props: {project :project}
-    }
-}
-
 const Project = ({project}) => {
     return(
         <>
